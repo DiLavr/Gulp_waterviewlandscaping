67dd6e535af0e95ef92c65ce0811be9bb01f58d1
DiLavr <42397553+Dilavr@user.noreply.github.com>
2020-06-06 02:16:39 -0400

    twentytwenty slider, generated css min, added js

---
+++ js/custon_add.js
@@ -0,0 +1,21 @@
+// initialize scrollspy
+$('body').scrollspy({
+
+    target: '.dotted-scrollspy'
+});
+
+// initialize lightbox
+$(function ()
+{
+
+    $("#mdb-lightbox-ui").load("../mdb-addons/mdb-lightbox-ui.html");
+});
+
+// $('.navbar-collapse a').click(function ()
+// {
+
+//     $(".navbar-collapse").collapse('hide');
+// });
+
+/* WOW.js init */
+new WOW().init();
